e22a48f1dfb733ecb338a51b6f36cd7c4c32df44
Mengqing <c897611977@gmail.com>
2023-07-09 16:42:03 +0100

    fix: mimetypes

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,6 +1,14 @@
+# Standard Library
+import mimetypes
+
+# Third-Party Libraries
 import pymysql
+
+# Project Imports
 from celery_main import app as celery_app
 
-__all__ = ('celery_app')
+__all__ = "celery_app"
 
 pymysql.install_as_MySQLdb()
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
