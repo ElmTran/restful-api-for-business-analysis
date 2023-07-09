ac461c1d5bfc1f34f61580b2aa5cc176f5f92e4f
Mengqing <c897611977@gmail.com>
2023-07-09 17:11:12 +0100

    fix: update mimetypes

--- settings/__init__.py
+++ settings/__init__.py
@@ -12,3 +12,4 @@ __all__ = "celery_app"
 pymysql.install_as_MySQLdb()
 mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/html", ".html", True)
